a0aa136af361cf5dee83b534b08402ae1818fe0f
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-04-20 09:00:35 +0530

    modified navbar

--- TechnovaganzaWebsite/src/App.jsx
+++ TechnovaganzaWebsite/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import StickyNavbar from './components/StickyNavbar'
+import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
+import Events from './components/Events';
+import Gallery from "./components/Gallery"
 import Banner from "./components/Banner";
 const App = () => {
   return (
     <div>
-      <StickyNavbar/>
+       <Router>
+      <StickyNavbar />
+       <Routes>
+        <Route path="Events" component={Events} />
+        <Route path="Gallery" component={Gallery} />
+        </Routes>
+    </Router>
+      {/* <StickyNavbar/> */}
       <Banner/>
     </div>
   )
--- TechnovaganzaWebsite/src/components/StickyNavbar.jsx
+++ TechnovaganzaWebsite/src/components/StickyNavbar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StickyNavbar = () => {
   return (
     <div className='sticky top-0'>
-      
-      <div className='flex justify-between  items-center  text-white  z-50 py-8 px-6 w-full bg-opacity-60 bg-[url(/src/assets/starbg.webp)]'>
-        <a href="#Home" className=' hover:text-yellow-500 
-        hover:underline'>LOGO</a>
-        
-        <a href="#Events" className='hover:text-yellow-500 hover:underline transition delay-150 duration-300 ease-in-out'>EVENTS</a>
-        <a href="#Gallery" className='hover:text-yellow-500 hover:underline transition delay-150 duration-300 ease-in-out'>GALLERY</a>
-        <div className='flex  w-1/6 space-x-20 right-0 items-center'>
-        <button className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-6 py-3 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'><a href="#Login">LOGIN</a></button>
-        <button className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900'><a href="#Register">REGISTER</a></button>
+      <nav className='flex justify-between items-center text-white z-50 py-8 px-6 w-full bg-opacity-60 bg-[url(/src/assets/starbg.webp)]'>
+        <ul className="flex space-x-4">
+          
+          <li>
+            <Link to="Events">Events</Link>
+          </li>
+          <li>
+            <Link to="Gallery">Gallery</Link>
+          </li>
+        </ul>
+        <div className='flex space-x-4'>
+          <button className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-6 py-3 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'>
+            <a href="#Login">LOGIN</a>
+          </button>
+          <button className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900'>
+            <a href="#Register">REGISTER</a>
+          </button>
         </div>
-      </div>
-     </div>
-  )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default StickyNavbar
-
+export default StickyNavbar;
